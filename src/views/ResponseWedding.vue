5e64b476c7f7b4b18313c0b3ac8e3ad4742909d0
Daniel Mendez <danielmenc@outlook.com>
2023-02-22 23:09:59 -0600

    #comment adding new main pages

---
+++ src/views/ResponseWedding.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-card class="pa-4">
+    <v-row justify="center">
+      <v-dialog
+        v-model="dialog"
+        persistent
+        width="auto"
+      >
+        <v-card>
+          <v-card-title class="text-h5">Hola inserte-nombre!</v-card-title>
+          <v-card-text class="text-center">Su numero de pases es inserte-numero.</v-card-text>
+          <v-card-text class="text-center">Gracias por confirmar.</v-card-text>
+          <!-- <span class="text-h3 pt-6 text-center" id="countdown"></span> -->
+          <v-card-text class="text-center">Esperamos emocionados ese gran día.</v-card-text>
+          <!-- <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="green-darken-1"
+              variant="text"
+              @click="dialog = false"
+            >
+              Entendido
+            </v-btn>
+          </v-card-actions> -->
+        </v-card>
+      </v-dialog>
+    </v-row>
+    <v-row>
+      <v-col
+        v-for="n in 20"
+        :key="n"
+        class="d-flex child-flex"
+        cols="3"
+      >
+        <v-img
+          :src="`/public/images/caroussel/${n}.jpg`"
+          aspect-ratio="1"
+          cover
+          :id="`index-${n}`"
+          class="bg-grey-lighten-2 rounded"
+        >
+          <template v-slot:placeholder>
+            <v-row
+              class="fill-height ma-0"
+              align="center"
+              justify="center"
+            >
+              <v-progress-circular
+                indeterminate
+                color="grey-lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      dialog: true,
+      images: [
+
+      ],
+    }
+  },
+}
+</script>
--- src/views/Wedding.vue
+++ src/views/Wedding.vue
@@ -1,118 +1,75 @@
 <template>
-  <!-- main picture -->
-  <div>
-    <v-img
-      src="/public/images/main-img.jpg"
-      max-height="100vh"
-      cover
-      class="bg-grey-lighten-2"
-    >
-      <v-card class="main-title">
-        <v-card-text class="text-center text-h5 pa-6">
-          <span class="font-italic text-h2">Daniel & Jhovana</span><br><br>
-          <span class="text-h4">¡Nos casamos!</span><br><br>
-          <span class="text-h3 pt-6" id="countdown"></span>
-        </v-card-text>
-      </v-card>
-    </v-img>
-  </div>
-
-  <!-- instructions modal -->
-  <div class="text-center">
-    <v-dialog
-      v-model="dialog"
-      width="auto"
-    >
-      <v-card>
-        <v-card-text class="text-center px-10">
-          <h1 class="text-red-lighten-1">¡Bienvenido!</h1><br>
-          <span class="text-h6">Esta página es para confirmar tu asistencia a<br> la boda.</span><br>
-          <span class="text-h6">Por favor siga las intrucciones al final de la<br> pagina para registrarse correctamente.</span><br>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn
-            color="success"
-            block
-            @click="dialog = false"
-            class="pb-6"
-          >Comencemos</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </div>
-
-  <!-- verse && grettings -->
-  <div class="text-center py-10">
-    <span
-      class="text-h4 font-italic text-grey-darken-2 pa-2"
-    >
-      "Asi que, no son ya más dos, si no una sola carne; por tanto, lo que Dios<br>ha unido no lo separe el hombre" - Mateo 19:6
-    </span>
-    <v-card class="main-color elevation-0">
-      <v-img
-        src="/public/images/abrazo.jpg"
-        max-height="6 0vh"
-        cover
-        class="ma-12 rounded"
-      >
-      </v-img>
-    </v-card>
-    <span
-      class="text-h5 font-italic text-grey-darken-2 pa-6"
-    >
-      Hola familia y/o amigos les enviamos un gran saludo y esperamos en el Señor<br> que se encuentren bien.
-      Nosotros estamos muy contentos y emocionados ya que este año cumpliremos<br> la promesa que nos hicimos un 31 de Diciembre Jhovana y yo. <br>Han sido meses de mucha espera pero el momento ha llegado, Gracias a Dios.<br>
-      Tenemos la dicha de invitarles este 30 de Diciembre a que nos acompañes a la celebración de nuestra boda,<br> de parte de Jhova & yo esperamos que puedas asistir.
-    </span>
-  </div>
-
-  <!-- button to details -->
-  <v-btn
-    align="center"
-  >
-    Detalle de la Boda
-  </v-btn>
+  <Main />
+  <InstructionModal />
+  <Verse />
+  <Details />
+  <Moments />
+  <Invitation />
+  <Plan />
+  <Map />
+  <Advice class="mt-10" />
+  <Answer class="mb-10" />
+  <!-- <Form :request="openForm"/> -->
+  <Footer />
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        dialog: true,
-      }
-    },
-  }
+import Main from "@/components/Main.vue";
+import InstructionModal from "@/components/InstructionModal.vue";
+import Verse from "@/components/Verse.vue";
+import Details from "@/components/ButtonDetails.vue";
+import Moments from "@/components/Moments.vue";
+import Invitation from "@/components/Invitation.vue";
+import Plan from "@/components/Plan.vue";
+import Map from "@/components/Map.vue";
+import Advice from "@/components/Advice.vue";
+import Answer from "@/components/Answer.vue";
+import Footer from "@/components/Footer.vue";
+import Form from "@/components/Form.vue";
 
-  var countDownDate = new Date("Feb 28, 2023 19:00:00").getTime()
-
-  var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("countdown").innerHTML = "Gracias por su asistencia!";
+export default {
+  components: {
+    Main,
+    InstructionModal,
+    Verse,
+    Details,
+    Moments,
+    Invitation,
+    Plan,
+    Map,
+    Advice,
+    Answer,
+    Form,
+    Footer
+  },
+  data() {
+    return {
+      openForm: true
     }
-  }, 1000);
+  },
+}
 </script>
-<style>
-  .main-title {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
-    background-color: black;
-    opacity: 0.7;
-    color: #FDFDBD;
-  }
+<style scoped>
+>>> .main-title {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+      background-color: black;
+      opacity: 0.7;
+      color: #FDFDBD;
+    }
 
-  .main-color {
-    background-color: #ffdddd;
-  }
+>>> .second-title {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+      color: white;
+    }
+
+>>> .main-color {
+      background-color: #ffdddd;
+    }
 </style>
