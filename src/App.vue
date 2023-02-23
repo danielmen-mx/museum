c61304367a89f2ea4bfcaed2c58cda63c9601ebf
Daniel Mendez <danielmenc@outlook.com>
2023-02-22 23:09:39 -0600

    #comment improve router

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,12 @@
     </div>
   </header> -->
 
-  <RouterView />
+  <!-- <RouterView /> -->
+  <div>
+    <router-view v-slot="{Component}" >
+      <component :is="Component" :key="$route.path"></component>
+    </router-view>
+  </div>
 </template>
 
 <script setup>
