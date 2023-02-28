b43d2cd01089c3ae685ed422bad4643578263dba
Daniel Mendez <danielmenc@outlook.com>
2023-02-28 09:58:23 -0600

    #comment create eventbus file

---
+++ src/utils/EventBus.js
@@ -0,0 +1,21 @@
+
+import Vue from 'vue'
+
+// class EventBus {
+//   constructor() {
+//     this.eventBus = new Vue()
+//   }
+
+//   fire(event, data = null) {
+//     this.vue.$emit(event, data)
+//   }
+
+//   listen(event, callback) {
+//     this.vue.$on(event, callback)
+//   }
+
+//   unlisten(event, callback) {
+//     this.vue.$off(event, callback)
+//   }
+// }
+export default new Vue()
