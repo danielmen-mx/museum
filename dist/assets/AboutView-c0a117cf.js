f000440c2d5ed0fc06b828be5b19128a6f33f4d6
Daniel Mendez <danielmenc@outlook.com>
2023-03-16 20:17:37 -0600

    deploy production 20:17:34

--- dist/assets/AboutView-c0a117cf.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o as t,c as o,b as s}from"./index-7fd9c34b.js";const _={},c={class:"about"},n=s("h1",null,"This is an about page",-1),a=[n];function r(i,u){return t(),o("div",c,a)}const f=e(_,[["render",r]]);export{f as default};
