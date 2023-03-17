169dd4a93828389dd4b80eb33e02b85935cf3d66
Daniel Mendez <danielmenc@outlook.com>
2023-03-17 00:40:21 -0600

    #comment improve the way toredirect to the manager

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,36 @@
 <template>
+  <!-- dialog -->
+  <div class="text-center">
+    <v-dialog
+      v-model="dialog"
+      activator="parent"
+      width="300"
+      height="170"
+    >
+      <v-card>
+        <v-text-field
+          :disabled="fail"
+          v-model="secret"
+          class="pa-2"
+          label="Key"
+          :hint="hint"
+          type="password"
+        >
+        </v-text-field>
+        <v-card-actions>
+          <v-btn
+            :disabled="fail"
+            :color="btn_color"
+            block
+            @click="handleSecretKey()"
+          >
+            {{btn_title}}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+
   <div class="main-color d-flex flex-column">
     <div class="d-flex w-100 align-center px-4 mb-1">
       <div>
@@ -31,6 +63,12 @@
 <script>
   export default {
     data: () => ({
+      secret: '',
+      hint: 'Enter your secret-key to access to the manager',
+      btn_title: 'send',
+      btn_color: 'success',
+      fail: false,
+      dialog: false,
       icons: [
         'mdi-facebook',
         'mdi-linkedin',
@@ -39,7 +77,29 @@
     }),
     methods: {
       redirect() {
-        this.$router.push({ path: '/admin'})
+        this.dialog = true
+      },
+      handleSecretKey() {
+        if (this.secret == '') return
+
+        if (this.secret.includes('secret-table')) {
+          this.dialog = false
+
+          setTimeout(() => {
+            this.$router.push({ path: '/admin'})
+          }, 3000);
+        } else {
+          this.fail = true
+          this.btn_color = 'red'
+          this.btn_title = "You're not the admin"
+          setTimeout(() => {
+            this.secret = ''
+            this.dialog = false
+            this.fail = false
+            this.btn_color = 'success'
+            this.btn_title = "send"
+          }, 2000);
+        }
       }
     },
   }
